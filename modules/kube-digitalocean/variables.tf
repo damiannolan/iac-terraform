3ee72d0051833ea0107f4d4d2e72c4c27e782e0d
Damian Nolan <damiannolan@gmail.com>
2020-10-11 17:28:16 +0200

    Creating dedicated outputs file for separtion of concerns

--- modules/kube-digitalocean/variables.tf
+++ modules/kube-digitalocean/variables.tf
@@ -37,7 +37,3 @@ variable "kubeconfig_path" {
   default     = "~/.kube/config"
 }
 
-output "kubernetes_host" {
-  description = "The hostname of the API server for the cluster"
-  value       = digitalocean_kubernetes_cluster.cluster.endpoint
-}
